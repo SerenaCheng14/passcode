d4b136426682cf59f2ef715fb6aa6d4f4cf1cc77
SerenaCheng <ccc9514789@gmail.com>
2020-11-27 17:42:34 +0800

    Initial Commit

---
+++ passcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  passcode
+//
+//  Created by Damon on 2020/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
